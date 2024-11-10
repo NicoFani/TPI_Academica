e5b2e999fd760b0bbef51209208bbe390493c18d
Franco Sanchez <francosanchez8102@gmail.com>
2024-11-10 20:36:55 -0300

    update: add validation to docentes cursos

--- Servicios/DocentesCursosService.cs
+++ Servicios/DocentesCursosService.cs
@@ -7,63 +7,67 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
-namespace Servicios
-{
-    public class DocentesCursoService(Context context)
-    {
-        public IEnumerable<DocentesCurso> GetDocentesCursos()
-        {
+namespace Servicios {
+    public class DocentesCursoService(Context context) {
+        public IEnumerable<DocentesCurso> GetDocentesCursos() {
             return context.DocentesCursos.Include(ai => ai.IdDocenteNavigation).Include(ai => ai.IdCursoNavigation!.IdMateriaNavigation!.IdPlanNavigation!.IdEspecialidadNavigation).Include(doCu => doCu.IdCursoNavigation!.IdComisionNavigation).ToList();
         }
 
-        public IEnumerable<DocentesCurso> GetDocentesCursosByCurso(int idCurso)
-        {
+        public IEnumerable<DocentesCurso> GetDocentesCursosByCurso(int idCurso) {
             return context.DocentesCursos.Include(ai => ai.IdDocenteNavigation).Where(ai => ai.IdCurso == idCurso).ToList();
         }
 
-        public IEnumerable<DocentesCurso> GetDocentesCursosByDocente(int idDocente)
-        {
+        public IEnumerable<DocentesCurso> GetDocentesCursosByDocente(int idDocente) {
             return context.DocentesCursos.Include(ai => ai.IdCursoNavigation).Where(ai => ai.IdDocente == idDocente).ToList();
         }
 
-        public DocentesCurso? GetDocenteCurso(int id)
-        {
+        public DocentesCurso? GetDocenteCurso(int id) {
             return context.DocentesCursos.Include(ai => ai.IdDocenteNavigation).Include(ai => ai.IdCursoNavigation).FirstOrDefault(ai => ai.IdDictado == id);
         }
 
-        public bool AddDocenteCurso(DocentesCurso docenteCurso)
-        {
+        public bool AddDocenteCurso(DocentesCurso docenteCurso) {
             Curso? curso = context.Cursos.Find(docenteCurso.IdCurso);
             Persona? docente = context.Personas.Find(docenteCurso.IdDocente);
-            if (curso == null || docente == null || docente.TipoPersona != "Profesor")
-            {
+            if (curso == null || docente == null || docente.TipoPersona != "Profesor" || (docenteCurso.Cargo != "Profesor" && docenteCurso.Cargo != "Auxiliar")) {
                 return false;
-            }
-            else
-            {
+            } else {
                 context.DocentesCursos.Add(docenteCurso);
                 context.SaveChanges();
                 return true;
             }
         }
 
-        public void UpdateDocenteCurso(DocentesCurso docenteCurso)
-        {
-            context.DocentesCursos.Update(docenteCurso);
-            context.SaveChanges();
+        public bool UpdateDocenteCurso(DocentesCurso docenteCurso) {
+            var old = context.DocentesCursos.Find(docenteCurso.IdDictado);
+            if (old != null && (docenteCurso.Cargo == "Profesor" || docenteCurso.Cargo == "Auxiliar")) {
+                if (old.IdDocente != docenteCurso.IdDocente) {
+                    var docente = context.Personas.Find(docenteCurso.IdDocente);
+                    if (docente == null || docente.TipoPersona != "Profesor") {
+                        return false;
+                    }
+                }
+                if (old.IdCurso != docenteCurso.IdCurso) {
+                    var curso = context.Cursos.Find(docenteCurso.IdCurso);
+                    if (curso == null) {
+                        return false;
+                    }
+                }
+                context.Entry(old).State = EntityState.Detached;
+                context.DocentesCursos.Update(docenteCurso);
+                context.SaveChanges();
+                return true;
+            } else {
+                return false;
+            }
         }
 
-        public bool DeleteDocenteCurso(int id)
-        {
+        public bool DeleteDocenteCurso(int id) {
             var docenteCurso = context.DocentesCursos.Find(id);
-            if (docenteCurso != null)
-            {
+            if (docenteCurso != null) {
                 context.DocentesCursos.Remove(docenteCurso);
                 context.SaveChanges();
                 return true;
-            }
-            else
-            {
+            } else {
                 return false;
             }
         }
