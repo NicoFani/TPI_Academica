58bae14034f79a44ad092b45be669d9dcc0f5dca
Franco Sanchez <francosanchez8102@gmail.com>
2024-11-11 10:05:07 -0300

    fix: bug with speciality form

--- UI.Desktop/specialityForm.cs
+++ UI.Desktop/specialityForm.cs
@@ -10,73 +10,51 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using UI.Desktop.Clients;
 
-namespace UI.Desktop
-{
-    public partial class specialityForm : Form
-    {
+namespace UI.Desktop {
+    public partial class specialityForm : Form {
         private Especialidade speciality;
         private ErrorProvider errorProvider = new ErrorProvider();
 
-        public Especialidade Speciality
-        {
+        public Especialidade Speciality {
             get { return speciality; }
-            set
-            {
+            set {
                 speciality = value;
                 this.SetSpeciality();
             }
         }
         public bool EditMode { get; set; }
 
-        public specialityForm()
-        {
+        public specialityForm() {
             InitializeComponent();
         }
-        private async void acceptButton_Click(object sender, EventArgs e)
-        {
-            if (this.ValidateSpeciality())
-            {
-                if (this.Speciality == null)
-                {
+        private async void acceptButton_Click(object sender, EventArgs e) {
+            if (this.ValidateSpeciality()) {
+                if (this.Speciality == null) {
                     this.Speciality = new Especialidade();
                 }
-
                 this.Speciality.DescEspecialidad = this.descTextBox.Text;
-                if (this.EditMode)
-                {
+                if (this.EditMode) {
                     await SpecialityApiClient.UpdateAsync(Speciality.IdEspecialidad, Speciality);
-                }
-                else
-                {
+                } else {
                     await SpecialityApiClient.AddAsync(this.Speciality);
                 }
+                this.Close();
             }
-            this.Close();
         }
 
-        private void cancelButton_Click(object sender, EventArgs e)
-        {
+        private void cancelButton_Click(object sender, EventArgs e) {
             this.Close();
         }
-        private void SetSpeciality()
-        {
-            if (this.Speciality != null)
-            {
+        private void SetSpeciality() {
+            if (this.Speciality != null) {
                 this.descTextBox.Text = this.Speciality.DescEspecialidad;
             }
-            else
-            {
-                // Manejar el caso donde Speciality es null
-                this.descTextBox.Text = string.Empty;
-            }
         }
 
-        private bool ValidateSpeciality()
-        {
+        private bool ValidateSpeciality() {
             bool isValid = true;
-            errorProvider.SetError(descTextBox, string.Empty);
-            if (this.descTextBox.Text == string.Empty)
-            {
+            errorProvider.SetError(descTextBox, "");
+            if (descTextBox.Text == "") {
                 errorProvider.SetError(descTextBox, "La descripción es requerida");
                 isValid = false;
             }
