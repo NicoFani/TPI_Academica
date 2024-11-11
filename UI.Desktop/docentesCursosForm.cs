30f622935b6b6c55df400f08bf20d3f1a690d146
Franco Sanchez <francosanchez8102@gmail.com>
2024-11-11 10:32:53 -0300

    fix: docentes cursos comboBoxs

--- UI.Desktop/docentesCursosForm.cs
+++ UI.Desktop/docentesCursosForm.cs
@@ -10,85 +10,64 @@ using System.Windows.Forms;
 using Datos.Models;
 using UI.Desktop.Clients;
 
-namespace UI.Desktop
-{
-    public partial class docentesCursosForm : Form
-    {
+namespace UI.Desktop {
+    public partial class docentesCursosForm : Form {
         private DocentesCurso _docenteCurso;
         private ErrorProvider errorProvider = new ErrorProvider();
 
-        public DocentesCurso DocenteCurso
-        {
+        public DocentesCurso DocenteCurso {
             get { return _docenteCurso; }
-            set
-            {
+            set {
                 _docenteCurso = value;
                 this.SetDocenteCurso();
             }
         }
         public bool EditMode { get; set; } = false;
-        public docentesCursosForm()
-        {
+        public docentesCursosForm() {
             InitializeComponent();
         }
-        private async void acceptButton_Click(object sender, EventArgs e)
-        {
-            if (this.ValidateDocenteCurso())
-            {
+        private async void acceptButton_Click(object sender, EventArgs e) {
+            if (this.ValidateDocenteCurso()) {
                 this.DocenteCurso.IdCurso = (int)comboBox1.SelectedValue;
                 this.DocenteCurso.IdDocente = (int)comboBox2.SelectedValue;
-                this.DocenteCurso.Cargo = textBox1.Text;
+                this.DocenteCurso.Cargo = cargoComboBox.SelectedItem.ToString();
                 this.DocenteCurso.IdCursoNavigation = null;
                 this.DocenteCurso.IdDocenteNavigation = null;
-                if (this.EditMode)
-                {
+                if (this.EditMode) {
                     await DocentesCursosApiClient.UpdateAsync(DocenteCurso);
-                }
-                else
-                {
+                } else {
                     await DocentesCursosApiClient.AddAsync(DocenteCurso);
                 }
             }
             this.Close();
         }
-        private void cancelButton_Click(object sender, EventArgs e)
-        {
+        private void cancelButton_Click(object sender, EventArgs e) {
             this.Close();
         }
 
-        private void SetDocenteCurso()
-        {
+        private void SetDocenteCurso() {
             this.comboBox1.SelectedValue = this.DocenteCurso.IdCurso;
             this.comboBox2.SelectedValue = this.DocenteCurso.IdDocente;
-            this.textBox1.Text = this.DocenteCurso.Cargo;
+            this.cargoComboBox.SelectedItem = this.DocenteCurso.Cargo;
         }
-        private bool ValidateDocenteCurso()
-        {
+
+        private bool ValidateDocenteCurso() {
             bool isValid = true;
 
             errorProvider.SetError(comboBox1, string.Empty);
             errorProvider.SetError(comboBox2, string.Empty);
-            errorProvider.SetError(textBox1, string.Empty);
 
-            if (this.comboBox1.SelectedValue == null)
-            {
+            if (this.comboBox1.SelectedValue == null) {
                 isValid = false;
-                errorProvider.SetError(comboBox1, "El curso es requerida");
+                errorProvider.SetError(comboBox1, "El curso es requerido");
             }
-            if (this.comboBox2.SelectedValue == null)
-            {
+            if (this.comboBox2.SelectedValue == null) {
                 isValid = false;
-                errorProvider.SetError(comboBox2, "El docente es requerida");
-            }
-            if (this.textBox1.Text == string.Empty)
-            {
-                isValid = false;
-                errorProvider.SetError(textBox1, "El cargo es requerido");
+                errorProvider.SetError(comboBox2, "El docente es requerido");
             }
             return isValid;
         }
-        private async void docentesCursosForm_Load(object sender, EventArgs e)
-        {
+        private async void docentesCursosForm_Load(object sender, EventArgs e) {
             string tipoPersona = "Profesor";
             var docentes = await PersonasApiClient.GetPersonasByTipoAsync(tipoPersona);
             var cursos = await CursosApiClient.GetCursosAsync();
@@ -107,12 +86,11 @@ namespace UI.Desktop
             comboBox2.DisplayMember = "Profesor";
             comboBox2.ValueMember = "IdDocente";
 
-            if (this.EditMode)
-            {
+            if (this.EditMode) {
                 comboBox1.Enabled = false;
                 comboBox2.Enabled = false;
-                comboBox1.SelectedValue = this.DocenteCurso.IdCurso;
-                comboBox2.SelectedValue = this.DocenteCurso.IdDocente;
+            } else {
+                cargoComboBox.SelectedItem = "Profesor";
             }
         }
 
